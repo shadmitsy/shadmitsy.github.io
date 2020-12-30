37c93995250e30d6849c51d684e76b2f5883f3f5
Shadmitsy <shadmitsy@gmail.com>
2020-12-30 22:26:03 +0200

    build2

---
+++ precache-manifest.bfd35c895aa2a4b83f080b4e0da47dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1c1e03f74d12e7691fd2b2f2736a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e57071d33d78d31dbe85",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "e05d25c5deee99bb10a0",
+    "url": "/static/css/main.d3354b1f.chunk.css"
+  },
+  {
+    "revision": "e57071d33d78d31dbe85",
+    "url": "/static/js/2.7a7803ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7a7803ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05d25c5deee99bb10a0",
+    "url": "/static/js/main.b611de1a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
